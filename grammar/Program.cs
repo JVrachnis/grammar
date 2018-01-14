5f2aa5d674d520353bbff61d5a96467c9795e170
johnythefoxGR <john.al.vra@hotmail.com>
2018-01-14 23:32:02 +0200

    Add project files.

---
+++ grammar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Grammar
+{
+    class Program
+    {
+        static string[] ekfrasi = { "<oros>", "<ekfrasi> + <oros>" };
+        static string[] oros = { "<paragontas>", "<oros> * <paragontas>" };
+        static string[] paragontas = { "a", "b", "c" };
+        static Dictionary<string, string[]> grammar = new Dictionary<string, string[]>();
+        static string input = "<ekfrasi>";
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            grammar.Add("<ekfrasi>", ekfrasi);
+            grammar.Add("<oros>", oros);
+            grammar.Add("<paragontas>", paragontas);
+            while (input.Contains("<"))
+            {
+                foreach (string Key in grammar.Keys)
+                {
+                    int place = -1;
+                    while (input.Contains(Key))
+                    {
+                        Console.WriteLine(input+"  =>");
+                        place = input.LastIndexOf(Key);
+                        input = input.Remove(place, Key.Length).Insert(place, grammar[Key][r.Next(grammar[Key].Length)]);
+                    }
+                }
+            }
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+    }
+}
